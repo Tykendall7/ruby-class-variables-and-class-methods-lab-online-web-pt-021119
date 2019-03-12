e1d5153d81b1edd84b0822b8d89a78c69f38566d
Tymeckia Kendall <tykend@gmail.com>
2019-03-12 23:50:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,12 +5,10 @@ class Song
                 
     @@count = 0 
     
-    def initialize
-      @@count +=1
-  end    
-  def self.count
-    @@count
-  end
-    
+  def initialize
+      @@count += 1
+  end   
+  
+
     
 end
